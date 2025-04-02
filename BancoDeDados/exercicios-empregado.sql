c1dc2a63f334f02761fd6a3f76aecabc1fd9aeae
deveduardo5 <eduardo.asouza23@senacsp.edu.br>
2025-04-01 22:12:20 -0300

    Delete BancoDeDados/exercicios-empregado.sql

--- BancoDeDados/exercicios-empregado.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists empregado (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    idade int(02) NOT NULL,
-    departamento varchar(50) not null,
-    salario decimal NOT NULL
-);
