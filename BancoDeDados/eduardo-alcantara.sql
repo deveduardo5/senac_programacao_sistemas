92ad09f9c5b84a9c6f990b29d306a83e8f5b869f
EDUARDO ALCANTARA DE SOUZA <eduardo.asouza23@senacsp.edu.br>
2025-04-07 21:57:58 -0300

    Teste Finalizado

--- BancoDeDados/eduardo-alcantara.sql
+++ BancoDeDados/eduardo-alcantara.sql
@@ -46,7 +46,7 @@ SELECT
 FROM
     cliente;
     
-insert into cliente (nome, cidade, idade) values ('João Silva', 'São Paulo', 35), ('Maria Santos', 'Belo Horizonte', 28), ('Carlos Lima ', 'Rio de Janeiro ', 42), ('Fernanda Rocha', 'Curitiba', 30);
+insert into cliente (nome, cidade, idade) values ('João Silva', 'São Paulo', 35), ('Maria Santos', 'Belo Horizonte', 28), ('Carlos Lima', 'Rio de Janeiro ', 42), ('Fernanda Rocha', 'Curitiba', 30), ('Lucas Ruiz', 'Curitiba', 35), ('Rodrigo Garro', 'Curitiba', 40), ('Galvão Bueno', 'Rio de Janeiro ', 60);
 
 CREATE TABLE IF NOT EXISTS pedido (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -74,33 +74,235 @@ insert into pedido (produto_id, quantidade, data_pedido, cliente_id) values ( 1,
 /*Consultas e Filtros (WHERE, ORDER BY, LIKE, BETWEEN)*/
 
 /*1. Selecione os produtos da categoria "Eletrônicos" cujo preço seja superior a R$3000, ordenados do mais caro para o mais barato.*/
-SELECT nome, categoria, preco FROM produto WHERE categoria = 'Eletrônicos' AND preco > 3000 ORDER BY nome ASC;
+SELECT 
+    nome, categoria, preco
+FROM
+    produto
+WHERE
+    categoria = 'Eletrônicos'
+        AND preco > 3000
+ORDER BY preco desc;
 
 /*2. Encontre os clientes que não moram em São Paulo e têm idade acima de 30 anos.*/
-SELECT * FROM cliente WHERE cidade <> 'São Paulo' and idade > 30;
+SELECT 
+    *
+FROM
+    cliente
+WHERE
+    cidade <> 'São Paulo' AND idade > 30;
 
 /*3. Liste os pedidos feitos entre "2024-03-10" e "2024-03-15", ordenados pela data em ordem crescente.*/
-SELECT * FROM pedido WHERE date(data_pedido) between '2024-03-10' AND '2024-03-15';
+SELECT 
+    *
+FROM
+    pedido
+WHERE
+    DATE(data_pedido) BETWEEN '2024-03-10' AND '2024-03-15' order by data_pedido asc; 
 
 /*4. Recupere os produtos cujo estoque é menor que 10 unidades e exiba em ordem crescente de estoque.*/
-SELECT nome, estoque FROM produto WHERE estoque < 10 ORDER BY estoque ASC;
+SELECT 
+    nome, estoque
+FROM
+    produto
+WHERE
+    estoque < 10
+ORDER BY estoque ASC;
 
 /*5. Liste os fornecedores que não estão no Rio de Janeiro e cujo nome começa com "T".*/
-SELECT * FROM fornecedor WHERE cidade != 'Rio de Janeiro' AND nome LIKE 'T%';
+SELECT 
+    *
+FROM
+    fornecedor
+WHERE
+    cidade != 'Rio de Janeiro'
+        AND nome LIKE 'T%';
+
+
 
 /*Agregação e Agrupamento (COUNT, SUM, AVG, GROUP BY)*/
 
 /*1. Calcule o preço médio dos produtos para cada categoria.*/
-SELECT categoria, AVG(preco) FROM produto GROUP BY categoria;
+SELECT 
+    categoria, AVG(preco) AS média
+FROM
+    produto
+GROUP BY categoria;
 
 /*2. Conte quantos pedidos cada cliente realizou.*/
-SELECT cliente_id, COUNT(quantidade) FROM pedido GROUP BY cliente_id;
+SELECT 
+    cliente_id, COUNT(quantidade) AS pedidos
+FROM
+    pedido
+GROUP BY cliente_id;
 
 /*3. Liste o total de produtos em estoque por categoria.*/
-select categoria, sum(estoque) from produto group by categoria;
+SELECT 
+    categoria, SUM(estoque) AS estoque
+FROM
+    produto
+GROUP BY categoria;
 
 /*4. Encontre o pedido com a maior quantidade de produtos comprados e exiba o id do
 pedido, produto_id e quantidade.*/
+SELECT 
+    id AS id_pedido, produto_id, SUM(quantidade)
+FROM
+    senac.pedido
+GROUP BY quantidade DESC
+LIMIT 1;
 
 /*5. Descubra quantos clientes há em cada cidade, ordenando os resultados da cidade com
 mais clientes para a cidade com menos clientes.*/
+SELECT 
+    COUNT(nome) AS cliente, cidade
+FROM
+    cliente
+GROUP BY cidade
+ORDER BY cliente DESC;
+
+
+
+/*Junções (INNER JOIN)*/
+
+/*1. Liste todos os produtos junto com o nome do fornecedor correspondente, ordenados
+pelo nome do fornecedor.*/
+SELECT 
+    p.nome AS produto, f.nome
+FROM
+    produto p
+        INNER JOIN
+    fornecedor f ON p.id_fornecedor = f.id
+ORDER BY f.nome ASC;
+
+/*2. Mostre os pedidos junto com o nome do cliente e o nome do produto comprado,
+ordenando pela data do pedido.*/
+SELECT 
+    pedido.id AS pedido,
+    cliente.nome AS nome,
+    produto.nome AS produto,
+    pedido.data_pedido
+FROM
+    pedido
+        INNER JOIN
+    cliente ON pedido.cliente_id = cliente.id
+        INNER JOIN
+    produto ON pedido.produto_id = produto.id
+ORDER BY pedido.data_pedido ASC;
+
+/*3. Liste todos os pedidos e exiba o nome do cliente, o nome do produto e o nome do
+fornecedor do produto.*/
+SELECT 
+    pedido.id AS pedido,
+    cliente.nome AS nome,
+    produto.nome AS produto,
+    fornecedor.nome AS fornecedor
+FROM
+    pedido
+        INNER JOIN
+    cliente ON pedido.cliente_id = cliente.id
+        INNER JOIN
+    produto ON pedido.produto_id = produto.id
+        INNER JOIN
+    fornecedor ON produto.id_fornecedor = fornecedor.id;
+
+/*4. Mostre a quantidade total de produtos comprados por cada cliente, listando o nome do
+cliente e a soma total de produtos adquiridos.*/
+SELECT 
+    cliente.nome AS nome, SUM(quantidade) AS quantidade
+FROM
+    pedido
+        INNER JOIN
+    cliente ON pedido.cliente_id = cliente.id
+GROUP BY cliente.nome;
+
+
+
+/*Subconsultas e Modificação de Dados (Subquery, UPDATE, DELETE,
+INSERT INTO)*/
+
+/*1. Encontre os produtos cujo preço é superior à média de todos os produtos da mesma
+categoria.*/
+SELECT 
+    *
+FROM
+    produto p
+WHERE
+    preco > (SELECT 
+            AVG(preco)
+        FROM
+            produto pr
+        WHERE
+            p.categoria = pr.categoria)
+ORDER BY categoria ASC;
+
+/*2. Aumente o preço dos produtos da categoria "Eletrônicos" em 10%.*/
+UPDATE produto 
+SET 
+    preco = 1.10 * preco
+WHERE
+    categoria = 'Eletrônicos';
+
+/*3. Remova os pedidos de clientes que moram em Curitiba.*/
+DELETE FROM pedido 
+WHERE
+    cliente_id IN (SELECT 
+        cidade
+    FROM
+        cliente
+    
+    WHERE
+        cidade = 'Curitiba');
+
+SELECT 
+    *
+FROM
+    pedido;
+
+/*4. Insira um novo cliente chamado "Ricardo Lopes", que mora em Porto Alegre e tem 38
+anos.*/
+insert into cliente (nome, cidade, idade) values ('Ricardo Lopes', 'Porto Alegre', 38);
+
+SELECT 
+    *
+FROM
+    cliente;
+
+/*5. Insira um novo pedido para o cliente "João Silva", comprando 2 unidades de "Notebook
+Y" no dia "2024-03-25".*/
+SELECT 
+    id
+FROM
+    profuto
+WHERE
+    nome = 'Notebook Y';
+
+SELECT 
+    *
+FROM
+    cliente
+WHERE
+    nome = 'joao silva'
+        AND cidade = 'sao paulo';
+
+insert into pedido (produto_id, quantidade, data_pedido, cliente_id) values (2, '2024-03-25', (select id from profuto where nome = 'Notebook Y'), (select * from cliente where nome = 'joao silva' and cidade = 'sao paulo'));
+
+/*6. Crie uma consulta que exiba o nome dos clientes que já compraram produtos da
+categoria "Móveis".*/
+SELECT 
+    cliente.nome,
+    produto.categoria,
+    pedido.id AS 'Número do Pedido',
+    produto.nome AS 'Nome do Produto'
+FROM
+    pedido
+        JOIN
+    cliente ON cliente_id = cliente.id
+        JOIN
+    produto ON produto_id = produto.id
+WHERE
+    produto.id = ANY (SELECT 
+            produto.id
+        FROM
+            produto
+        WHERE
+            categoria = 'Móveis');
