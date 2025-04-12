b478eb4f54b84b4a063f4b89f4052c3e7fbab6e1
EDUARDO ALCANTARA DE SOUZA <eduardo.asouza23@senacsp.edu.br>
2025-04-11 22:20:22 -0300

    Projeto Novo

--- ListaAtividades/obj/Debug/net8.0-windows/ListaAtividades.AssemblyInfo.cs
+++ ListaAtividades/obj/Debug/net8.0-windows/ListaAtividades.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CadastroCliente")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ListaAtividades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eecaa6b70cec272bd6b759f897debeff541b25f8")]
-[assembly: System.Reflection.AssemblyProductAttribute("CadastroCliente")]
-[assembly: System.Reflection.AssemblyTitleAttribute("CadastroCliente")]
+[assembly: System.Reflection.AssemblyProductAttribute("ListaAtividades")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ListaAtividades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
