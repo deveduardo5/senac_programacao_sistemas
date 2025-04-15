05ac394430873f010455dbf8f146d4f43b7a9c9d
EDUARDO ALCANTARA DE SOUZA <eduardo.asouza23@senacsp.edu.br>
2025-04-14 22:08:16 -0300

    Adicionando Métodos

--- ListaAtividades/obj/Debug/net8.0-windows/ListaAtividades.AssemblyInfo.cs
+++ ListaAtividades/obj/Debug/net8.0-windows/ListaAtividades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListaAtividades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eecaa6b70cec272bd6b759f897debeff541b25f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b478eb4f54b84b4a063f4b89f4052c3e7fbab6e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListaAtividades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListaAtividades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
